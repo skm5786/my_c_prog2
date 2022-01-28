ceb2777fade2c89991b993a7536ba00a1f1433bf
Sumit <sumitthakral3@gmail.com>
2022-01-28 14:25:28 +0530

    commit2

---
+++ exercise 5/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  exercise 5
+//
+//  Created by sumit thakral on 28/01/22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
